10b9015b8de398e11d2612686149544e22989bbe
Aleksei Gusev <aleksei.gusev@gmail.com>
2020-03-28 15:30:54 +0200

    Rename test files

--- src/__tests__/integration.js
+++ src/__tests__/integration.js
@@ -2,7 +2,7 @@ const {runMonurlSync} = require('./runMonurl')
 
 test('starts checks and logs progress', async () => {
   const result = runMonurlSync('..', ['--once'])
-  // console.log(result.stderr)
+
   expect(result.stderr).toEqual('')
   expect(result.stdout).toMatch('Starting monitoring')
 })
